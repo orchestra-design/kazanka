d63894e0cdf7aa9ffc5c6395e361806d2382af94
Alexei Accio <alexei.accio@gmail.com>
2019-10-08 16:25:43 +0300

    favicon

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Global } from '@emotion/core'
+import { useFavicon } from 'react-use'
 
 import '../../fonts/sofia/stylesheet.css'
 import './globals.css'
@@ -7,7 +8,11 @@ import './globals.css'
 import globalStyles from './global'
 import { layoutStyles } from './styles'
 
+import * as favicon from './favicon.png'
+
 export function Layout({ children }) {
+  useFavicon(favicon)
+
   return (
     <div css={layoutStyles}>
       <Global styles={globalStyles} />
